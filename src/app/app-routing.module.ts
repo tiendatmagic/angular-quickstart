c25ab143034c91da1df87204e099ad04f43df808
tiendatmagic <tiendatmagic8@gmail.com>
2022-11-17 23:51:23 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbcComponent } from './abc/abc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'abc', component: AbcComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
